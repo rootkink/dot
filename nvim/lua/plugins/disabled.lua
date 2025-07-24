654affde450a515c28f73a0702c6926ba5e15456
rootkink <wastedthisemail@gmail.com>
2025-07-24 22:42:30 +0530

    Update disabled.lua

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,12 @@
--- ~/.config/nvim/lua/plugins/disabled.lua
+
 return {
+
+  -- NOTE: Only one of the following UI plugins should be active at a time: `alpha-nvim`, `mini.starter`, or `dashboard.nvim` if u have imported "lazyvim.plugins.extras.ui"
+  -- These plugins serve the same purpose, so the other two should be disabled to prevent conflicts or redundancy.
+
+  { "goolord/alpha-nvim", enabled = false },
+  { "echasnovski/mini.starter", enabled = false },
+
 
   { "folke/flash.nvim", enabled = false },
 }
